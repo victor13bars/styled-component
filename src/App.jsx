c8d33592857db41d2c3f6242e7f15c17d4e87368
victor13bars <victor_bars_bars95@mail.ru>
2021-10-18 11:48:57 +0300

    Пропсы и псевдоклассы.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import styled from 'styled-components'
+import Title from "./components/Title";
+import Flex from "./components/Flex";
+import Console from "./components/Console";
+import Button from "./components/Button";
 
-function App() {
-  return (
-    <div>
-     APP
-    </div>
-  );
+const AppWrapper = styled.div`
+    width:100%;
+    min-height:100vh;
+    padding:2rem;
+    background:black;
+`
+
+const App = () => {
+    return (
+        <AppWrapper>
+            <Flex justify={'center'}>
+                <Title color={'red'}>Console cmd 2021. Victor</Title>
+            </Flex>
+            <Flex direction={'column'}>
+                <Console/>
+                <Button align={'flex-end'}>Отправить</Button>
+            </Flex>
+        </AppWrapper>
+    )
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import styled, {createGlobalStyle} from 'styled-components'
 
+const Global = createGlobalStyle`
+* {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    font-family:consolas
+}
+`
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <>
+        <Global/>
+        <App/>
+    </>,
+    document.getElementById('root')
 );
 
